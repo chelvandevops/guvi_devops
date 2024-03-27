328b69dcd4cac894eff81cf1e8320c10c3119985
Ubuntu <ubuntu@ip-172-31-24-101.ap-southeast-2.compute.internal>
2024-03-27 07:57:49 +0000

    adding the httpstatuscode.sh script to day7 task folder

---
+++ day7/httpstatuscode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "Enter the website to check the HTTP error code: " URL
+http_code=$(curl --write-out %{http_code} --silent --output /dev/null $URL)
+
+if [ $http_code -eq 200 ]; then
+        echo "The Response code of $URL is : $http_code"
+        echo "The HTTP connection is Success"
+else
+        echo "The Response code of $URL is : $http_code"
+        echo "The HTTP connection is Failure"
+fi
